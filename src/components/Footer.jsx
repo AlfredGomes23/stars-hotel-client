f595d8e33b1ff42bca651f374ed51b695402aaad
Alfred Gomes <alfredgomes23@users.noreply.github.com>
2023-11-07 00:07:36 +0600

    responsive footer added.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,27 @@
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <footer>
+            <div className="footer p-10 bg-base-300 mb-1 rounded-lg shadow-sm flex flex-col md:flex-row justify-center items-center">
+                <aside className="mx-auto text-center">
+                    <img className="w-24 mx-auto" src="https://i.ibb.co/BBDQrqN/logo.png" alt="" />
+                    <p>Stars Hotel Ltd.<br />Providing reliable service since 1992</p>
+                </aside>
+                <div className="flex gap-5 link-primary text-md md:text-lg mx-auto">
+                    <Link to="/">Home</Link>
+                    <Link to="/rooms">Rooms</Link>
+                    <Link to="/my-bookings">My Bookings</Link>
+                    <Link to="/about-us">About Us</Link>
+                    <Link to="/faq">FAQ</Link>
+                </div>
+            </div>
+            <div className="footer footer-center p-4 bg-base-300 text-base-content rounded-lg shadow-md mb-3">
+                <aside>
+                    <p>Copyright © 2023 - All right reserved by Stars Hotel Ltd.</p>
+                </aside>
+            </div>
+        </footer>
     );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,13 @@ const Navbar = () => {
         <li><NavLink to='/faq'>FAQ</NavLink></li>
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className={"navbar lg:mt-5 shadow-lg bg-base-300 rounded-lg mb-3"}>
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-fit">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-36">
                         {links}
                     </ul>
                 </div>
