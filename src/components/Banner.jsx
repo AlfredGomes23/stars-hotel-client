379e81773408cee3f0bec8650bccef9312381279
Alfred Gomes <alfredgomes23@users.noreply.github.com>
2023-11-07 19:49:16 +0600

    banner added.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -16,7 +16,21 @@ const Banner = () => {
     }, []);
     return (
         <div className='grid grid-cols-1 lg:grid-cols-2'>
-            
+            <div className=' mx-auto rounded-lg overflow-hidden lg:h-[50vh] flex flex-col justify-center mb-10 px-10 text-center' data-aos="slide-right">
+                <h3 className="text-xl mb-3 text-accent text-left">Welcome to,</h3>
+                <h1 className='text-5xl text-primary mb-10'>Stars Hotel</h1>
+                <p className='text-secondary text-3xl'>Experience the Perfect Blend of Luxury and Comfort at Our Hotel - Where Every Stay Feels Like Home</p>
+            </div>
+            <div className='w-full mb-5 lg:mb-12 mx-auto rounded-lg overflow-hidden' data-aos="slide-left">
+                <AutoplaySlider
+                    play={true}
+                    cancelOnInteraction={false} // should stop playing on user interaction
+                    interval={3000}
+                >
+                    <div data-src="https://i.ibb.co/FmcfYpF/Education.png" />
+                    <div data-src="https://i.ibb.co/FmcfYpF/Education.png" />
+                    <div data-src="https://i.ibb.co/FmcfYpF/Education.png" />
+                </AutoplaySlider></div>
         </div>);
 };
 
